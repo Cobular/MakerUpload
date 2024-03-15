6b1bf257deb183cc66a1812b3cf308aea0b27a63
Julie <22972550+Cobular@users.noreply.github.com>
2024-03-14 17:04:57 -0700

    Try something else

--- host/src-tauri/src/main.rs
+++ host/src-tauri/src/main.rs
@@ -21,6 +21,16 @@ fn main() {
                 .targets([LogTarget::LogDir, LogTarget::Stdout, LogTarget::Webview])
                 .build(),
         );
+
+        builder
+        .setup(|app| {
+            let window = app.get_window("main").unwrap();
+            set_shadow(&window, true).expect("Unsupported platform!");
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![download_file])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
     }
 
     builder
